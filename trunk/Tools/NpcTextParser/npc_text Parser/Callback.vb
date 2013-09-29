2c30dcc321507f640306b67b0990549a7ccee520
necrosummon <necrosummon@2ad69eca-7e5b-c0b3-efbc-283e626d773c>
2013-09-29 16:37:49 +0000

    Uploaded Tools

---
+++ trunk/Tools/NpcTextParser/npc_text Parser/Callback.vb
@@ -0,0 +1,50 @@
+﻿Module Callback
+
+    Delegate Sub SetTextCallback(ByVal obj As Object, ByVal [text] As String)
+    Delegate Sub AddObjectCallback(ByVal obj As Object, ByVal obj2 As Object)
+    Delegate Sub SetValueCallback(ByVal obj As Object)
+    Delegate Sub SetVisibleCallback(ByVal obj As Object)
+    Delegate Sub SetNoVisibleCallback(ByVal obj As Object)
+
+    Public Sub SetVisible(ByVal obj As Object)
+        If obj.InvokeRequired Then
+            Dim d As New SetVisibleCallback(AddressOf SetVisible)
+            Form1.Invoke(d, New Object() {obj})
+        Else
+            obj.Visible = True
+        End If
+    End Sub
+    Public Sub SetNoVisible(ByVal obj As Object)
+        If obj.InvokeRequired Then
+            Dim d As New SetNoVisibleCallback(AddressOf SetNoVisible)
+            Form1.Invoke(d, New Object() {obj})
+        Else
+            obj.Visible = False
+        End If
+    End Sub
+    Public Sub SetText(ByVal obj As Object, ByVal [text] As String)
+        If obj.InvokeRequired Then
+            Dim d As New SetTextCallback(AddressOf SetText)
+            Form1.Invoke(d, New Object() {obj, [text]})
+        Else
+            obj.Text = [text]
+        End If
+    End Sub
+    Public Sub AddObj(ByVal obj As Object, ByVal obj2 As Object)
+        If obj.InvokeRequired Then
+            Dim d As New AddObjectCallback(AddressOf AddObj)
+            Form1.Invoke(d, New Object() {obj, obj2})
+        Else
+            obj2.Parent = obj
+        End If
+    End Sub
+    Public Sub SetValue(ByVal obj As Object)
+        If obj.InvokeRequired Then
+            Dim d As New SetValueCallback(AddressOf SetValue)
+            Form1.Invoke(d, New Object() {obj})
+        Else
+            obj.VerticalScroll.Value = obj.VerticalScroll.Maximum
+            obj.ScrollControlIntoView(obj)
+        End If
+    End Sub
+End Module
---
+++ trunk/Tools/NpcTextParser/npc_text Parser/Notification.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Net
+Imports System.Text
+Imports System.IO
+
+Public Class Notification
+    Private uri, text1, text2 As String
+
+    Public Sub New(str1 As String, str2 As String, id As String)
+        text1 = str1
+        text2 = str2
+        uri = id
+    End Sub
+
+    Public Function sendToast() As String
+        Try
+            Dim sendNotificationRequest As HttpWebRequest = DirectCast(WebRequest.Create(uri), HttpWebRequest)
+            sendNotificationRequest.Method = "POST"
+            Dim toastMessage As String = "<?xml version=""1.0"" encoding=""utf-8""?>" & "<wp:Notification xmlns:wp=""WPNotification"">" & "<wp:Toast>" & "<wp:Text1>" & text1 & "</wp:Text1>" & "<wp:Text2>" & text2 & "</wp:Text2>" & "</wp:Toast> " & "</wp:Notification>"
+            Dim notificationMessage As Byte() = Encoding.[Default].GetBytes(toastMessage)
+            sendNotificationRequest.ContentLength = notificationMessage.Length
+            sendNotificationRequest.ContentType = "text/xml"
+            sendNotificationRequest.Headers.Add("X-WindowsPhone-Target", "toast")
+            sendNotificationRequest.Headers.Add("X-NotificationClass", "2")
+            Using requestStream As Stream = sendNotificationRequest.GetRequestStream()
+                requestStream.Write(notificationMessage, 0, notificationMessage.Length)
+            End Using
+            Dim response As HttpWebResponse = DirectCast(sendNotificationRequest.GetResponse(), HttpWebResponse)
+            Dim notificationStatus As String = response.Headers("X-NotificationStatus")
+            Dim notificationChannelStatus As String = response.Headers("X-SubscriptionStatus")
+            Dim deviceConnectionStatus As String = response.Headers("X-DeviceConnectionStatus")
+            Return "Notification Status: " & notificationStatus & vbCrLf & "Subscription Status: " & notificationChannelStatus & vbCrLf & "Connection Status: " & deviceConnectionStatus
+        Catch ex As Exception
+            MsgBox("Exception caught sending update: " & ex.ToString())
+        End Try
+        Return vbEmpty
+    End Function
+End Class
