c6330291ea15790e080af936c26d1cb570ba77a3
necrosummon <necrosummon@2ad69eca-7e5b-c0b3-efbc-283e626d773c>
2013-10-03 12:09:59 +0000

    [WAREMU] Updated WAREMU Launcher

--- WAREMU/ServerLauncher/Form1.Designer.cs
+++ WAREMU/ServerLauncher/Form1.Designer.cs
@@ -26,29 +26,30 @@
         /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
         /// le contenu de cette méthode avec l'éditeur de code.
         /// </summary>
-        private void InitializeComponent()
+                private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.B_start = new System.Windows.Forms.Button();
             this.B_stop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // B_start
             // 
-            this.B_start.Location = new System.Drawing.Point(12, 12);
+            this.B_start.Location = new System.Drawing.Point(92, 170);
             this.B_start.Name = "B_start";
             this.B_start.Size = new System.Drawing.Size(75, 23);
             this.B_start.TabIndex = 1;
-            this.B_start.Text = "Lancer";
+            this.B_start.Text = "Start";
             this.B_start.UseVisualStyleBackColor = true;
             this.B_start.Click += new System.EventHandler(this.B_start_Click);
             // 
             // B_stop
             // 
-            this.B_stop.Location = new System.Drawing.Point(94, 12);
+            this.B_stop.Location = new System.Drawing.Point(332, 170);
             this.B_stop.Name = "B_stop";
             this.B_stop.Size = new System.Drawing.Size(75, 23);
             this.B_stop.TabIndex = 2;
-            this.B_stop.Text = "Arretter";
+            this.B_stop.Text = "Close";
             this.B_stop.UseVisualStyleBackColor = true;
             this.B_stop.Click += new System.EventHandler(this.B_stop_Click);
             // 
@@ -56,11 +57,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(182, 42);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(494, 214);
             this.Controls.Add(this.B_stop);
             this.Controls.Add(this.B_start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "ServerLauncher";
+            this.Text = "WAREMU Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
